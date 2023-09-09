668b8a9cbbdc5fc74bfec87e4665ebd65a922882
KanaOyaizu <143867420+kanao0@users.noreply.github.com>
2023-09-09 08:36:19 +0000

    [finish]bookers topページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get 'homes/top'
+  resources :books
+  root :to => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+  
+  # get '/books' => 'books#new'
+  # get '/books' => 'books#index'
+  # get '/books' => 'books#show'
+  # get '/books' => 'books#edit'
+  # get '/' => 'homes#top'
