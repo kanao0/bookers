b168acd1858aa2706b60fefbaa6df262a89107e4
KanaOyaizu <143867420+kanao0@users.noreply.github.com>
2023-09-10 15:56:24 +0000

    [in the middle of]editページ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,20 +1,15 @@
 class BooksController < ApplicationController
-  # 要らない
-  # def new
-  # end
-
   def create
     # 保存するための箱を作る名前はbook
     book = Book.new(book_params)
 
-# データベースにデータ保存する
     book.save
-# データベースに保存が終わったら詳細画面へ行くbookの→showアクション
-    # redirect_to prefix_path(渡す.id)
+    
     redirect_to book_path(book.id)
 
-
   end
+
+
 
   def index
     # データを入れるための箱を作る
@@ -22,30 +17,52 @@ class BooksController < ApplicationController
     # データ表示したい、データを入れた箱(@booksと名前つけた)を作る
     @books = Book.all
   end
+  
 
   def show
-    # Bookモデルのデータみつけてきてーデータの箱は@bookって名前にしたよ
+    # Bookモデルのデータみつけてきて～データの箱は@bookって名前にしたよ
     @book = Book.find(params[:id])
   end
+  
 
   def edit
-    # book = Book.find(params[:id])  # データ（レコード）を1件取得
-    # book.destroy  # データ（レコード）を削除
-    # redirect_to books_path  # 投稿一覧画面へリダイレクト  
+    # Bookモデルのデータをみつけてきて～editページに
+    @book = Book.find(params[:id])
   end
+  
+  def update
+    book = Book.find(params[:id])
+    
+    book.update(book_params)
+    
+    redirect_to book_path(book.id)  
+  end
+
   
   def destroy
     book = Book.find(params[:id])  # データ（レコード）を1件取得
-    
-    book.destroy  # データ（レコード）を削除
+    # 探してきたデータ（レコード）を削除
+    book.destroy  
     redirect_to books_path  # 投稿一覧画面へリダイレクト  
   end
 
-private
+  private
   # ストロングパラメータ
   def book_params
     params.require(:book).permit(:title, :body)
   end
 
-
 end
+
+
+  # def create
+  #   # 保存するための箱を作る名前はbook
+
+  # end
+  
+    #   @book = Book.new(book_params)
+    # if @book.save
+    #   redirect_to book_path(@book.id)
+    # else
+    #   render :index
+    # end
