82c8cdb67298eec6ee54a4af1fd3b47516a9696b
KanaOyaizu <143867420+kanao0@users.noreply.github.com>
2023-09-11 05:04:17 +0000

    [finish]create,update失敗メッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,19 @@
 class BooksController < ApplicationController
   def create
-    # 保存するための箱を作る名前はbook
-    book = Book.new(book_params)
+    # データを保存するための箱を作る名前は@book
+    @book = Book.new(@book_params)
 
-    book.save
-    
-    redirect_to book_path(book.id)
-
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
 
-
   def index
-    # データを入れるための箱を作る
+    # 新規投稿のデータを入れるための箱を作る名前は@book
     @book = Book.new
     # データ表示したい、データを入れた箱(@booksと名前つけた)を作る
     @books = Book.all
@@ -29,13 +30,18 @@ class BooksController < ApplicationController
     # Bookモデルのデータをみつけてきて～editページに
     @book = Book.find(params[:id])
   end
+
   
   def update
-    book = Book.find(params[:id])
+    # updateされたデータを入れる箱つくる
+    @book = Book.find(params[:id])
     
-    book.update(book_params)
+    if @book.update(book_params)
     
-    redirect_to book_path(book.id)  
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   
@@ -55,14 +61,24 @@ class BooksController < ApplicationController
 end
 
 
-  # def create
-  #   # 保存するための箱を作る名前はbook
 
-  # end
+
+    # def create
+    #   # 保存するための箱を作る名前はbook
+    #   book = Book.new(book_params)
   
-    #   @book = Book.new(book_params)
-    # if @book.save
-    #   redirect_to book_path(@book.id)
-    # else
-    #   render :index
+    #   book.save
+      
+    #   redirect_to book_path(book.id)
+  
     # end
+    
+    
+      def update
+    updateされたデータを入れる箱
+    book = Book.find(params[:id])
+    
+    book.update(book_params)
+    
+    redirect_to book_path(book.id)  
+  end
